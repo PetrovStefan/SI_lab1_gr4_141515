acf21ce8c269a2bea6df9756550ee7196b081938
Stefan Petrov <stefanbt8@gmail.com>
2020-04-12 22:59:35 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -3,11 +3,41 @@ class Point {
 	String color;
 	double x,y;
 
-	//TODO add new variable
-
 	//TODO constructor
-
+	Point(){
+		System.out.println("This is a no argument constructor");
+	}
 	//TODO setters and getters
+
+	public void setCoordinates(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public void getX(){
+		return x;
+	}
+
+	public void getY(){
+		return y;
+	}
+
+	public void setColor(String color){
+		this.color = color;
+	}
+
+	public void getColor(){
+		return color;
+	}
+
+	public void move(char xDirection, char yDirection){
+		this.x = xDirection;
+		this.y = yDirection;
+	}
+
+	public void draw(){
+		System.out.println(x, " ", y)
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
